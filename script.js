82ca8d4929502b6784d4059681053af147ebf425
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-16 14:41:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const price = document.querySelectorAll("[data-ns-test]")
+const table = document.querySelector("table")
+	let total = 0
+console.log(total)
+	let tr = document.createElement("tr")
+	let td  = document.createElement("td")
+	let td1 = td.cloneNode()
+	tr.appendChild(td)
+	tr.appendChild(td1)
+	console.log(tr)
