986cc523234aabeaf3d125fb9ac6228bb80ebe1c
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-16 20:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //your code here
+const body = document.querySelector("body")
 const price = document.querySelectorAll(`[data-ns-test="price"]`)
 const table = document.querySelector("table")
 const tbody = document.querySelector("tbody")
-let length = price.length
+const btn = document.createElement("button")
+btn.textContent = "Submit"
+body.appendChild(btn)
+	let length = price.length
 	let tr = document.createElement("tr")
 	let td  = document.createElement("td")
 	let td1 = td.cloneNode()
@@ -12,12 +16,14 @@ let length = price.length
 	tr.appendChild(td1)
    td1.setAttribute("data-ns-test","grandTotal")
     tbody.appendChild(tr)
-	td1["data-ns-test"] = 0
-for(let i=0; i<length; i++){
-	td1["data-ns-test"] = Number(td1["data-ns-test"])  + (Number(price[i].textContent))
-	price[i]["data-ns-test"] = `${price[i].textContent}`
+const handeler = (e)=>{
+	console.log(e)
+   let total = 0
+	for(let i=0; i<length; i++){
+	total = total  + (Number(price[i].textContent))
+	// price[i]["data-ns-test"] = `${price[i].textContent}`
+	}
+	td1.textContent = `${total}`
+	td1["data-ns-test"] = `${total}`
 }
-let total = Number(td1["data-ns-test"])
-    td1.textContent = Number(`${total}`)
-td1["data-ns-test"] = Number(`${total}`)
-console.log(table)
+btn.addEventListener("click",handeler)
