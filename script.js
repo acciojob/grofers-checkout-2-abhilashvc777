0e37d6f800667f6e0e1209ab79a2ebf8e0ee5112
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-16 14:47:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //your code here
 const price = document.querySelectorAll("[data-ns-test]")
 const table = document.querySelector("table")
+const tbody = document.querySelector("tbody")
 	let total = 0
-console.log(total)
+for(let i=0; i<price.length; i++){
+	total = total + (parseFloat(price[i].textContent))
+}
 	let tr = document.createElement("tr")
 	let td  = document.createElement("td")
 	let td1 = td.cloneNode()
 	tr.appendChild(td)
+    td.textContent = "total"
 	tr.appendChild(td1)
+    td1.textContent = `${total}`
+    tbody.appendChild(tr)
 	console.log(tr)
